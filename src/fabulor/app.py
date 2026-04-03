42c7a3e2b5f70df41d11493c967fd07eeaf3fcfc
PrymeJ <prymesinistajinx@gmail.com>
2026-04-04 01:16:20 +0300

    Add basic app entry point and project configuration

--- src/fabulor/app.py
+++ src/fabulor/app.py
@@ -1,2 +1,11 @@
+import sys
+from PySide6.QtWidgets import QApplication, QLabel
+
 def run():
-    print("Fabulor starting...")
+    app = QApplication(sys.argv)
+
+    label = QLabel("Fabulor")
+    label.resize(300, 100)
+    label.show()
+
+    sys.exit(app.exec())
